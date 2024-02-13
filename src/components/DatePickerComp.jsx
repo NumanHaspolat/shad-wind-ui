147dc0eb95ff29ad7916ddb0fa09dfc1f39488ff
Numan Haspolat <haspolatnuman@gmail.com>
2024-02-13 17:13:59 +0300

    date

--- src/components/DatePickerComp.jsx
+++ src/components/DatePickerComp.jsx
@@ -2,16 +2,34 @@ import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import "../App.css";
-
 const DatePickerComp = () => {
   const [startDate, setStartDate] = useState(new Date());
 
+  const renderDayContents = (day, date) => {
+    const isDaySelected =
+      startDate.getDate() === date.getDate() &&
+      startDate.getMonth() === date.getMonth();
+    return (
+      <div className={isDaySelected ? "selected-day" : "non-selected-day"}>
+        {day}
+      </div>
+    );
+  };
+
   return (
-    <div className="date-picker">
-      <p className="color-light p-3 fs-2">Pick a date</p>
+    <div className="date-picker-container">
+      <p
+        className="color=light p-3 fs-2"
+        style={{
+          marginBottom: ".5rem",
+        }}
+      >
+        Pick a date
+      </p>
       <DatePicker
         selected={startDate}
         onChange={(date) => setStartDate(date)}
+        renderDayContents={renderDayContents}
       />
     </div>
   );
